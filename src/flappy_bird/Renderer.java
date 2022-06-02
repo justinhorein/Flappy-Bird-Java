69c743047217a9aa70a7287b439879b1db78c3c2
Justin Horein <justinhorein@gmail.com>
2022-06-01 22:18:44 -0700

    infinite columns working

---
+++ src/flappy_bird/Renderer.java
@@ -0,0 +1,17 @@
+package flappy_bird;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		FlappyBird.repaint(g);
+	}
+}
