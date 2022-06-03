c433d242fc34c14eaa304920acacb476363e64b4
Justin Horein <justinhorein@gmail.com>
2022-06-02 22:31:05 -0700

    game working! still need to add score

--- src/flappy_bird/FlappyBird.java
+++ src/flappy_bird/FlappyBird.java
@@ -1,10 +1,7 @@
 package flappy_bird;
 
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Rectangle;
-import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -12,7 +9,7 @@ import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class FlappyBird implements ActionListener
+public class FlappyBird implements ActionListener, KeyListener
 {
 	
 	public static FlappyBird FlappyBird;
@@ -23,11 +20,13 @@ public class FlappyBird implements ActionListener
 	
 	public static Rectangle bird;
 	
-	public int ticks, yMotion;
+	public int ticks, yMotion, score;
 	
 	public static ArrayList<Rectangle> columns;
 	
 	public Random rand;
+
+	public static boolean gameOver, started;
 	
 	public FlappyBird()
 	{
@@ -41,12 +40,13 @@ public class FlappyBird implements ActionListener
 		jframe.setTitle("Flappy Bird");
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setSize(WIDTH, HEIGHT);
+		jframe.addKeyListener(this);
 		jframe.setResizable(false);
 		jframe.setVisible(true);
 		
 		bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
 		columns = new ArrayList<Rectangle>();
-		
+
 		addColumn(true);
 		addColumn(true);
 		addColumn(true);
@@ -54,38 +54,80 @@ public class FlappyBird implements ActionListener
 		
 		timer.start();
 	}
+
+	public void jump() {
+		if (gameOver) {
+			bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
+			columns.clear();
+			yMotion = 0;
+			score = 0;
+
+			addColumn(true);
+			addColumn(true);
+			addColumn(true);
+			addColumn(true);
+
+			gameOver = false;
+		}
+		if (!started) {
+			started = true;
+		} else if (!gameOver) {
+			if (yMotion > 0)
+			{
+				yMotion = 0;
+			}
+
+			yMotion -= 10;
+		}
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		int speed = 10;
 		
 		ticks++;
-		
-		for (int i = 0; i < columns.size(); i++) {
-			Rectangle column = columns.get(i);
-			column.x -= speed;
-		}
-		
-		if (ticks % 2 == 0 && yMotion < 15) {
-			yMotion += 2;
-		}
-		
-		for (int i = 0; i < columns.size(); i++) {
-			Rectangle column = columns.get(i);
-			if (column.x + column.width < 0)
-			{
-				columns.remove(column);
-				
-				if (column.y == 0) {
-					addColumn(false);
+
+		if (started) {
+
+			for (int i = 0; i < columns.size(); i++) {
+				Rectangle column = columns.get(i);
+				column.x -= speed;
+			}
+
+			if (ticks % 2 == 0 && yMotion < 15) {
+				yMotion += 2;
+			}
+
+			for (int i = 0; i < columns.size(); i++) {
+				Rectangle column = columns.get(i);
+				if (column.x + column.width < 0) {
+					columns.remove(column);
+
+					if (column.y == 0) {
+						addColumn(false);
+					}
 				}
 			}
+
+			bird.y += yMotion;
+
+			for (Rectangle column : columns) {
+				if (column.intersects(bird)) {
+					gameOver = true;
+
+					bird.x = column.x - bird.width;
+				}
+			}
+			if (bird.y > HEIGHT - 120 - bird.height || bird.y < 0) {
+				gameOver = true;
+			}
+
+			if (bird.y + yMotion >= HEIGHT - 120) {
+				bird.y = HEIGHT - 120 - bird.height;
+			}
 		}
-		
-		bird.y += yMotion;
-		
+
 		renderer.repaint();
-		
 	}
 	
 	public void addColumn(boolean start)
@@ -131,10 +173,39 @@ public class FlappyBird implements ActionListener
 		{
 			paintColumn(g, column);
 		}
+
+		g.setColor(Color.white);
+		g.setFont(new Font("Arial", 1, 100));
+
+		if (!started)
+		{
+			g.drawString("Press key to start!", 100, HEIGHT / 2 - 50);
+		}
+
+		if (gameOver)
+		{
+			g.drawString("Game Over!", 100, HEIGHT / 2 - 50);
+		}
 	}
 	
 	public static void main(String[] args) 
 	{
 		FlappyBird = new FlappyBird();
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		jump();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
 	}
 }
